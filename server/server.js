070d41ec0d37f0342d5e5a4991618fac382bd6f8
Gary Clark <gclark04432@live.co.uk>
2020-01-25 12:45:35 +0000

    add server.js

---
+++ server/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+
+const cors = require('cors');
+app.use(cors());
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+
+const MongoClient = require('mongodb').MongoClient;
+const createRouter = require('./helpers/create_router.js');
+
+
+MongoClient.connect('mongodb://localhost:27017')
+  .then((client) => {
+    const db = client.db('habit_tracker');
+    const habitsCollection = db.collection('habits');
+    const habitsRouter = createRouter(habitsCollection);
+    app.use('/api/habits', habitsRouter);
+  })
+  .catch(console.err);
+
+app.listen(3000, function () {
+  console.log(`Listening on port ${ this.address().port }`);
+});
