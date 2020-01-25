4d978e1e8369440d01a9206c46ca313243e25f24
Gary Clark <gclark04432@live.co.uk>
2020-01-25 13:10:05 +0000

    add update function to create_server.js

--- server/helpers/create_router.js
+++ server/helpers/create_router.js
@@ -15,39 +15,39 @@ const createRouter = function (collection) {
 
   router.get('/', (req, res) => {
     collection
-      .find()
-      .toArray()
-      .then((docs) => res.json(docs))
-      .catch((err) => {
-        console.error(err);
-        res.status(500);
-        res.json({ status: 500, error: err });
-      });
+    .find()
+    .toArray()
+    .then((docs) => res.json(docs))
+    .catch((err) => {
+      console.error(err);
+      res.status(500);
+      res.json({ status: 500, error: err });
+    });
   });
 
   router.get('/:id', (req, res) => {
     const id = req.params.id;
     collection
-      .findOne({ _id: ObjectID(id) })
-      .then((doc) => res.json(doc))
-      .catch((err) => {
-        console.error(err);
-        res.status(500);
-        res.json({ status: 500, error: err });
-      });
+    .findOne({ _id: ObjectID(id) })
+    .then((doc) => res.json(doc))
+    .catch((err) => {
+      console.error(err);
+      res.status(500);
+      res.json({ status: 500, error: err });
+    });
   });
 
   router.delete('/:id', (req, res) => {
     const id = req.params.id;
     collection
-      .deleteOne({ _id: ObjectID(id) })
-      .then(() => collection.find().toArray())
-      .then((docs) => res.json(docs))
-      .catch((err) => {
-        console.error(err);
-        res.status(500);
-        res.json({ status: 500, error: err });
-      });
+    .deleteOne({ _id: ObjectID(id) })
+    .then(() => collection.find().toArray())
+    .then((docs) => res.json(docs))
+    .catch((err) => {
+      console.error(err);
+      res.status(500);
+      res.json({ status: 500, error: err });
+    });
   });
 
   router.post('/', (req, res) => {
@@ -56,6 +56,25 @@ const createRouter = function (collection) {
     .insertOne(newData)
     .then((result) => {
       res.json(result.ops[0])
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500);
+      res.json({ status: 500, error: err });
+    });
+  });
+
+  router.put('/:id', (req, res) => {
+    const id = req.params.id;
+    const updatedData = req.body;
+    collection
+    .findOneAndUpdate(
+      { _id: ObjectID(id)},
+      { $set: updatedData },
+      {returnOriginal: false}
+    )
+    .then((result) => {
+      res.json(result.value)
     })
     .catch((err) => {
       console.error(err);
