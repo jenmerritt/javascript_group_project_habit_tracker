f326fbccfd86db65a3df2fe805732959d838287f
Gary Clark <gclark04432@live.co.uk>
2020-01-28 11:40:28 +0000

    refactor all timestamps to be Date object and finish conditional period method

--- server/db/seeds.js
+++ server/db/seeds.js
@@ -6,14 +6,14 @@ db.habits.insertMany([
     name: "Drink 3l Water",
     points: 20,
     timesAchieved: 0,
-    timeStamps: [],
+    timeStamps: ["2020-01-20", "2020-01-25", "2020-01-26", "2020-01-27"],
     period: "Daily"
   },
   {
     name: "Walk To Work",
     points: 25,
     timesAchieved: 2,
-    timeStamps: [],
+    timeStamps: ["2020-01-28"],
     period: "Daily"
   },
   {
@@ -27,14 +27,14 @@ db.habits.insertMany([
     name: "Play Football",
     points: 30,
     timesAchieved: 0,
-    timeStamps: [],
+    timeStamps: ["2020-01-22"],
     period: "Weekly"
   },
   {
     name: "Climb a Mountain",
     points: 50,
     timesAchieved: 0,
-    timeStamps: [],
+    timeStamps: ["2019-12-20"],
     period: "Monthly"
   }
 ]);
